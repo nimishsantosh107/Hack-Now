0ea2f87d456d3be5b17123312cb1d3c6bc8836b6
kavyajaganathan <43740147+kavyajaganathan@users.noreply.github.com>
2020-04-24 22:11:50 +0530

    fourth

--- app/src/main/java/com/example/hack_now/MainActivity.java
+++ app/src/main/java/com/example/hack_now/MainActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    int flag = 1;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
