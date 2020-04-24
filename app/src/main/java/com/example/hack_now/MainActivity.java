c272ec5e6caf77a78e782d04b349969b6d37c5e1
nimish <nimishsantosh107@icloud.com>
2020-04-24 19:27:18 +0530

    updated

--- app/src/main/java/com/example/hack_now/MainActivity.java
+++ app/src/main/java/com/example/hack_now/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.hack_now;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    int flag = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
