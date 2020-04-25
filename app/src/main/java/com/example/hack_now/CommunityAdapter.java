a7de5004cd429b2b11ebf532c70ea216877b7e0a
Sanjay Pokkali <sanjay.pokkali99@gmail.com>
2020-04-25 14:42:09 +0530

    News and Community list made

---
+++ app/src/main/java/com/example/hack_now/CommunityAdapter.java
@@ -0,0 +1,50 @@
+package com.example.hack_now;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommunityAdapter extends ArrayAdapter {
+    List<String> data=new ArrayList<>();
+    Context mContext;
+
+    public CommunityAdapter(Context context, int resource, List<String> data) {
+        super(context, resource, data);
+        mContext=context;
+
+        this.data=data;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view;
+        LayoutInflater inflater = (LayoutInflater) mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null) {
+
+            view = new View(mContext);
+            view = inflater.inflate(R.layout.community_card, null);
+
+
+
+            TextView title = (TextView) view.findViewById(R.id.title);
+
+            //set description
+            title.setText(data.get(position));
+        }
+        else
+        {
+            view = (View) convertView;
+        }
+        return view;
+    }
+}
--- app/src/main/java/com/example/hack_now/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/hack_now/ui/notifications/NotificationsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,13 +13,32 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.hack_now.CommunityAdapter;
 import com.example.hack_now.R;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class NotificationsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        //TextView textView=root.findViewById(R.id.test);
+
+        //Do get request
+
+
+
+
+
+        //After get request and we get json ready
+
+        List<String> test=Arrays.asList("Nimish","Kavya","Sanjay");
+        CommunityAdapter communityAdapter=new CommunityAdapter(getContext(), 0, test);
+        ListView communityList=root.findViewById(R.id.communityList);
+
+        communityList.setAdapter(communityAdapter);
         return root;
     }
 }
